65b7f5e7601674480a13dd86d3d3ba84ec11fee4
Abhinandan <abhi.moudgil15@gmail.com>
2021-05-15 00:32:40 +0530

    Incorporated Materialize CSS

--- client/src/components/Contest.js
+++ client/src/components/Contest.js
@@ -2,6 +2,7 @@ import React from "react";
 const Hero = () => {
   return (
     <div>
+      
     </div>
   );
 };
--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,8 +1,23 @@
 import React from "react";
+import { useEffect, useState } from "react";
+
 import '../css/hero.css';
+import M from 'materialize-css';
+
 const Hero = () => {
   return (
     <div>
+      <div>
+        <a id="menu" className="waves-effect waves-light btn btn-floating"><i className="material-icons">menu</i></a>
+        {/* Tap Target Structure */}
+        <div className="tap-target" data-target="menu">
+          <div className="tap-target-content">
+            <h5>Title</h5>
+            <p>A bunch of text</p>
+          </div>
+        </div>
+      </div>
+
       <video autoPlay={true} muted loop>
         <source src="/resources/logo_animation.mp4" type="video/mp4" />
       </video>
