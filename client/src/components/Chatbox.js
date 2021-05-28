a486b4c41a9b702302000f1cc7b86cbbb71b88fa
shubhangjyotirmay <57489798+shubhangjyotirmay@users.noreply.github.com>
2021-05-28 13:45:04 +0530

    add manual user handle

--- client/src/components/Chatbox.js
+++ client/src/components/Chatbox.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 // import { useEffect, useState } from "react";
 // import logo_light from "../resources/logo_light.png"
 // import heroVideo from "../resources/hero-video.mp4"
@@ -8,8 +8,44 @@ import "../css/hero.css";
 import M from "materialize-css";
 
 const Hero = () => {
+  useEffect(() => {
+    let options = {
+      placeholder: "Enter User Handle",
+      secondaryPlaceholder: "More Handles?"
+    };
+    document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('.chips');
+      const options = {
+        placeholder: "Enter User Handle",
+        secondaryPlaceholder: "More Handles?",
+      };
+      var instance = M.Chips.init(elems, options);
+      document.querySelector('.chips-placeholder').chips({
+        placeholder: "Enter User Handle",
+        secondaryPlaceholder: "More Handles?"
+      });
+      // var instance = M.Chips.getInstance(elems);
+      instance.addChip({
+        tag: 'chip content',
+        image: '', // optional
+      });
+    });
+    M.AutoInit()
+  }, []);
+
   return (
-    <div className="chat-box">
+    <div className="add-handles">
+      <div className="manual-handle">
+        <h4>Add User Handles</h4>
+      </div>
+  <div className="chips" />
+  <div className="get-questions"> 
+  <button className="waves-effect waves-light btn questions-button">
+    Get Questions
+  </button>
+
+  </div>
+
 </div>
 
   );
---
+++ client/src/components/chip.js
@@ -0,0 +1,39 @@
+//   $(document).ready(function(e) {   
+
+//  });
+
+// import M from "materialize-css";
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     var elems = document.querySelectorAll('.chips');
+//     var instances = M.Chips.init(elems, options);
+// });
+
+
+// window.onload = function () {
+//     $('.chips').chips();
+//     $('.chips-initial').chips({
+//         data: [{
+//             tag: 'Apple',
+//         }, {
+//             tag: 'Microsoft',
+//         }, {
+//             tag: 'Google',
+//         }],
+//     });
+//     $('.chips-placeholder').chips({
+//         placeholder: 'Enter a tag',
+//         secondaryPlaceholder: '+Tag',
+//     });
+//     $('.chips-autocomplete').chips({
+//         autocompleteOptions: {
+//             data: {
+//                 'Apple': null,
+//                 'Microsoft': null,
+//                 'Google': null
+//             },
+//             limit: Infinity,
+//             minLength: 1
+//         }
+//     });
+// }
