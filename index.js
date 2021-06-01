b2290568fccaf717836af038d02a73d480eb486c
Abhinandan <abhi.moudgil15@gmail.com>
2021-06-01 13:27:05 +0530

    Removed duplicacy in div1 and div2

--- index.js
+++ index.js
@@ -104,8 +104,9 @@ io.on("connection", (socket) => {
                 let handle_name1 = users[i];
                 // async function getSetGo() {
                 let modified_url = `https://codeforces.com/api/user.status?handle=${handle_name1}`;
-                
-                try{
+
+                try {
+                  console.log("hey")
                   const jsondata = await fetch(modified_url);
                   const jsdata = await jsondata.json();
                   console.log(jsdata.status);
@@ -118,6 +119,7 @@ io.on("connection", (socket) => {
                         jsdata.result[i].problem.contestId +
                         "-" +
                         jsdata.result[i].problem.index;
+                        names.push(jsdata.result[i].problem.name);
                       solved.add(str);
                     }
                   }
@@ -127,12 +129,14 @@ io.on("connection", (socket) => {
                   for (let i = 0; i < jsdata2.result.length; i++) {
                     contests_given.add(jsdata2.result[i].contestId);
                   }
-                }catch{(err)=>{
-                  io.to(user.room).emit(
-                    "start_loader",
-                    "Error getting user submissions for"+handle_name1
-                  );
-                }}
+                } catch {
+                  (err) => {
+                    io.to(user.room).emit(
+                      "start_loader",
+                      "Error getting user submissions for" + handle_name1
+                    );
+                  };
+                }
               }
               io.to(user.room).emit(
                 "start_loader",
@@ -140,120 +144,130 @@ io.on("connection", (socket) => {
               );
 
               console.log("all users fetched");
+              try {
+                let modified_url2 = `https://codeforces.com/api/problemset.problems`;
+                const jsondata4 = await fetch(modified_url2);
+                console.log(jsdata4)
+                let jsdata4 = await jsondata4.json();
+                console.log("problems done");
+                let jsdataP = jsdata4;
+                let upsolved = [];
+                let upsolved2 = [];
+                function shuffle(array) {
+                  var currentIndex = array.length,
+                    temporaryValue,
+                    randomIndex;
 
-              let modified_url2 = `https://codeforces.com/api/problemset.problems`;
-              const jsondata4 = await fetch(modified_url2);
-              let jsdata4 = await jsondata4.json();
-              console.log("problems done");
-              let jsdataP = jsdata4;
-              let upsolved = [];
-              let upsolved2 = [];
-              function shuffle(array) {
-                var currentIndex = array.length,
-                  temporaryValue,
-                  randomIndex;
+                  // While there remain elements to shuffle...
+                  while (0 !== currentIndex) {
+                    // Pick a remaining element...
+                    randomIndex = Math.floor(Math.random() * currentIndex);
+                    currentIndex -= 1;
 
-                // While there remain elements to shuffle...
-                while (0 !== currentIndex) {
-                  // Pick a remaining element...
-                  randomIndex = Math.floor(Math.random() * currentIndex);
-                  currentIndex -= 1;
-
-                  // And swap it with the current element.
-                  temporaryValue = array[currentIndex];
-                  array[currentIndex] = array[randomIndex];
-                  array[randomIndex] = temporaryValue;
-                }
-
-                return array;
-              }
-              io.to(user.room).emit(
-                "start_loader",
-                "Retreiving Upsolving Questions...Done"
-              );
-              //Upsolving Retreival
-              for (let i = 0; i < jsdataP.result.problems.length; i++) {
-                if (
-                  contests_given.has(jsdataP.result.problems[i].contestId) &&
-                  solved.has(
-                    `${jsdataP.result.problems[i].contestId}-${jsdataP.result.problems[i].index}`
-                  ) == false
-                ) {
-                  let rating =
-                    jsdataP.result.problems[i].rating != undefined
-                      ? jsdataP.result.problems[i].rating
-                      : 9999999999;
-                  upsolved.push([
-                    rating,
-                    `${jsdataP.result.problems[i].contestId}-${jsdataP.result.problems[i].index}`,
-                    jsdataP.result.problems[i].name,
-                  ]);
-                  upsolved2.push([
-                    rating,
-                    `${jsdataP.result.problems[i].contestId}-${jsdataP.result.problems[i].index}`,
-                    jsdataP.result.problems[i].name,
-                  ]);
-                }
-              }
-              upsolved.sort();
-              upsolved2.sort();
-              let j = 0;
-              // shuffle(jsdata4.result.problems);
-              for (let I = 0; I < num; I++) {
-                let fl = false;
-                for (let i = 0; i < upsolved.length; i++) {
-                  if (
-                    upsolved[i][0] >= diff[I] &&
-                    upsolved[i][0] <= diff[I + 1] &&
-                    problems.includes(upsolved[i]) == false &&
-                    names.includes(upsolved2[i][2]) == false &&
-                    solved.has(upsolved[i][1]) === false
-                  ) {
-                    problems.push(upsolved[i]);
-                    names.push(upsolved2[i][2]);
-                    fl = 1;
-                    break;
+                    // And swap it with the current element.
+                    temporaryValue = array[currentIndex];
+                    array[currentIndex] = array[randomIndex];
+                    array[randomIndex] = temporaryValue;
                   }
+
+                  return array;
                 }
-                if (fl) {
-                  continue;
-                }
-                for (let i = 0; i < jsdata4.result.problems.length; i++) {
-                  let str =
-                    jsdata4.result.problems[i].contestId +
-                    "-" +
-                    jsdata4.result.problems[i].index;
-                  if (
-                    jsdata4.result.problems[i].rating >= diff[I] &&
-                    jsdata4.result.problems[i].rating <= diff[I + 1] &&
-                    solved.has(str) === false &&
-                    jsdata4.result.problemStatistics[i].solvedCount >= 900 &&
-                    jsdata4.result.problems[i].tags.includes("*special") ===
-                      false &&
-                    problems.includes([
-                      jsdata4.result.problems[i].rating,
-                      str,
-                    ]) == false &&
-                    names.includes(jsdata4.result.problems[i].name) == false
-                  ) {
-                    problems.push([jsdata4.result.problems[i].rating, str]);
-                    names.push(jsdata4.result.problems[i].name);
-                    break;
-                  }
-                }
-              }
-              console.log(names);
-              addProblems(user.room, problems).then((data) => {
                 io.to(user.room).emit(
                   "start_loader",
-                  "Adding Problems to database...Done"
+                  "Retreiving Upsolving Questions...Done"
                 );
-                console.log("problems to firebase done");
-                io.to(user.room).emit("start_contest", {
-                  problems: problems,
-                  room: user.room,
+                //Upsolving Retreival
+                for (let i = 0; i < jsdataP.result.problems.length; i++) {
+                  if (
+                    contests_given.has(jsdataP.result.problems[i].contestId) &&
+                    solved.has(
+                      `${jsdataP.result.problems[i].contestId}-${jsdataP.result.problems[i].index}`
+                    ) == false
+                  ) {
+                    let rating =
+                      jsdataP.result.problems[i].rating != undefined
+                        ? jsdataP.result.problems[i].rating
+                        : 9999999999;
+                    upsolved.push([
+                      rating,
+                      `${jsdataP.result.problems[i].contestId}-${jsdataP.result.problems[i].index}`,
+                      jsdataP.result.problems[i].name,
+                    ]);
+                    upsolved2.push([
+                      rating,
+                      `${jsdataP.result.problems[i].contestId}-${jsdataP.result.problems[i].index}`,
+                      jsdataP.result.problems[i].name,
+                    ]);
+                  }
+                }
+                upsolved.sort();
+                upsolved2.sort();
+                let j = 0;
+                // shuffle(jsdata4.result.problems);
+                for (let I = 0; I < num; I++) {
+                  let fl = false;
+                  for (let i = 0; i < upsolved.length; i++) {
+                    if (
+                      upsolved[i][0] >= diff[I] &&
+                      upsolved[i][0] <= diff[I + 1] &&
+                      problems.includes(upsolved[i]) == false &&
+                      names.includes(upsolved2[i][2]) == false &&
+                      solved.has(upsolved[i][1]) === false
+                    ) {
+                      problems.push(upsolved[i]);
+                      names.push(upsolved2[i][2]);
+                      fl = 1;
+                      break;
+                    }
+                  }
+                  if (fl) {
+                    continue;
+                  }
+                  for (let i = 0; i < jsdata4.result.problems.length; i++) {
+                    let str =
+                      jsdata4.result.problems[i].contestId +
+                      "-" +
+                      jsdata4.result.problems[i].index;
+                    if (
+                      jsdata4.result.problems[i].rating >= diff[I] &&
+                      jsdata4.result.problems[i].rating <= diff[I + 1] &&
+                      solved.has(str) === false &&
+                      jsdata4.result.problemStatistics[i].solvedCount >= 900 &&
+                      jsdata4.result.problems[i].tags.includes("*special") ===
+                        false &&
+                      problems.includes([
+                        jsdata4.result.problems[i].rating,
+                        str,
+                      ]) == false &&
+                      names.includes(jsdata4.result.problems[i].name) == false
+                    ) {
+                      problems.push([jsdata4.result.problems[i].rating, str]);
+                      names.push(jsdata4.result.problems[i].name);
+                      break;
+                    }
+                  }
+                }
+                console.log(names);
+                addProblems(user.room, problems).then((data) => {
+                  io.to(user.room).emit(
+                    "start_loader",
+                    "Adding Problems to database...Done"
+                  );
+                  console.log("problems to firebase done");
+                  io.to(user.room).emit("start_contest", {
+                    problems: problems,
+                    room: user.room,
+                  });
                 });
-              });
+              } catch {
+                (err) => {
+                  io.to(user.room).emit(
+                    "start_loader",
+                    "Error getting problems from codeforces"
+                  );
+                  console.log(err);
+                };
+              }
             }
             getFinal();
           });
